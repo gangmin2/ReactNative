09a02af1a4e67ef9747af6fcd247a07f82b87d9f
min <96777845+gangmin2@users.noreply.github.com>
2022-05-24 01:10:58 +0900

    flexDirection 예제

--- root/Layout/3_flexDirection.js
+++ root/Layout/3_flexDirection.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+
+const TestView = ({ direction, color }) => {
+    return (
+        <View style={[styles.container, {backgroundColor: `${color}`}]}>
+            <Text style={styles.text}>FlexDirection: '{direction}'</Text>
+            <View style={{flexDirection: `${direction}`}}>
+                <View style={styles.box}></View>
+                <View style={styles.box}></View>
+                <View style={styles.box}></View>
+            </View>
+        </View>
+    )
+}
 
 const FlexDirection = ({ navigation }) => {
     return (
-        <View style={styles.container}>
-            <Text>FlexDirection</Text>
-        </View>
+        <ScrollView>
+            <Text style={styles.titleText}>FlexDirection</Text>
+            <TestView direction={"column"} color={'lightpink'}></TestView>
+            <TestView direction={"row"} color={'lightskyblue'}></TestView>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: 'white',
+        flex: 1
+    },
+    box: {
+        backgroundColor: 'red',
+        width: 80,
+        height: 80,
+        margin: 20
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: '900',
+        margin: 20
+    },
+    text: {
+        fontSize: 20,
+        margin: 20,
+        marginBottom: 0,
+        fontWeight: '900'
     }
 })
 
