789e555749c5538c4d514872395d5519a71100ae
min <96777845+gangmin2@users.noreply.github.com>
2022-05-24 00:55:25 +0900

    flex 예제

--- root/Layout/1_flex.js
+++ root/Layout/1_flex.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Flex = ({ navigation }) => {
     return (
-        <View style={styles.container}>
-            <Text>Flex</Text>
+        <View style={{flex: 1}}>
+            <View style={[styles.container, {flex: 1, backgroundColor: "pink"}]}>
+                <Text style={styles.text}>flex: 1</Text>
+            </View>
+            <View style={[styles.container, {flex: 2, backgroundColor: "skyblue"}]}>
+                <Text style={styles.text}>flex: 2</Text>
+            </View>
+            <View style={[styles.container, {flex: 5, backgroundColor: "palegoldenrod"}]}>
+                <Text style={styles.text}>flex: 5</Text>
+            </View>
+            <View style={[styles.container, {flex: 3, backgroundColor: "palegreen"}]}>
+                <Text style={styles.text}>flex: 3</Text>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 20
     }
 })
 
