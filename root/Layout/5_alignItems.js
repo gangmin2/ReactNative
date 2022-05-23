2eaa5fa3b9f1ab690d8e86adf71963776efdd0a5
min <96777845+gangmin2@users.noreply.github.com>
2022-05-24 01:53:23 +0900

    alignItems 예제

--- root/Layout/5_alignItems.js
+++ root/Layout/5_alignItems.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const TestView = ({ value, color, count }) => {
+    if (count) {
+        return (
+            <View style={{backgroundColor: `${color}`, flex: 1}}>
+                <Text style={styles.text}>AlignItems: '{value}'</Text>
+                <View style={[styles.container, {alignItems: `${value}`}]}>
+                    <View style={styles.box}></View>
+                    <View style={styles.box}></View>
+                    <View style={styles.box}></View>
+                </View>
+            </View>
+        )
+    } else {
+        return (
+            <View style={{backgroundColor: `${color}`, flex: 1}}>
+                <Text style={styles.text}>AlignItems: '{value}'</Text>
+                <View style={[styles.container, {alignItems: `${value}`}]}>
+                    <View style={styles.box}></View>
+                </View>
+            </View>
+        )
+    }
+}
 
 const AlignItems = ({ navigation }) => {
+    const [value, setValue] = useState("center")
+    const [selected, setSelected] = useState(0)
+    const [count, setCount] = useState(false)
+
     return (
         <View style={styles.container}>
-            <Text>AlignItems</Text>
+            <Text style={styles.titleText}>AlignItems</Text>
+            <Button title="single" onPress={() => setCount(false)} color={count ? 'skyblue' : 'blue'}/>
+            <Button title="multi" onPress={() => setCount(true)} color={count ? 'blue' : 'skyblue'}/>
+            <View style={{marginTop: 20}}>
+                <View style={{flexDirection: 'row'}}>
+                    <Button title="baseline" onPress={() => [setValue("baseline"), setSelected(0)]} color={selected == 0 ? 'blue' : 'skyblue'}/>
+                    <Button title="center" onPress={() => [setValue("center"), setSelected(1)]} color={selected == 1 ? 'blue' : 'skyblue'}/>
+                    <Button title="flex-start" onPress={() => [setValue("flex-start"), setSelected(2)]} color={selected == 2 ? 'blue' : 'skyblue'}/>
+                    <Button title="flex-end" onPress={() => [setValue("flex-end"), setSelected(3)]} color={selected == 3 ? 'blue' : 'skyblue'}/>
+                    <Button title="stretch" onPress={() => [setValue("stretch"), setSelected(4)]} color={selected == 4 ? 'blue' : 'skyblue'}/>
+                </View>
+                <View  style={{flexDirection: 'row', marginTop: 20}}>
+                </View>
+            </View>
+            <TestView value={value} color={'skyblue'} count={count}> </TestView>
         </View>
     )
 }
@@ -12,8 +54,24 @@ const AlignItems = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white',
-    }
+    },
+    box: {
+        backgroundColor: 'blue',
+        width: 80,
+        height: 80,
+        margin: 20
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: '900',
+        margin: 20
+    },
+    text: {
+        fontSize: 20,
+        margin: 20,
+        marginBottom: 0,
+        fontWeight: '900',
+    },
 })
 
 export default AlignItems
