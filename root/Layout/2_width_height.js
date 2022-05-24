5cf162e9339428585da553f2dea2df52b1990386
min <96777845+gangmin2@users.noreply.github.com>
2022-05-24 14:34:08 +0900

    width & height 예제

--- root/Layout/2_width_height.js
+++ root/Layout/2_width_height.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const WidthandHeight = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Text>WidthandHeight</Text>
+        <Text style={styles.titleText}>Width &amp; Height</Text>
+        <View style={[styles.box, {width: 100, height: 100}]}>
+            <Text style={styles.contentText}>width: 100</Text>
+            <Text style={styles.contentText}>height: 100</Text>
+        </View>
+        <View style={[styles.box, {width: 150, height: 100}]}>
+            <Text style={styles.contentText}>width: 150</Text>
+            <Text style={styles.contentText}>height: 100</Text>
+        </View>
+        <View style={[styles.box, {width: 100, height: 80}]}>
+            <Text style={styles.contentText}>width: 100</Text>
+            <Text style={styles.contentText}>height: 80</Text>
+        </View>
+        <View style={[styles.box, {width: 300, height: 150}]}>
+            <Text style={styles.contentText}>width: 300</Text>
+            <Text style={styles.contentText}>height: 150</Text>
+        </View>
         </View>
     )
 }
@@ -13,6 +29,22 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
+    },
+    box: {
+        backgroundColor: 'red',
+        margin: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: '900',
+        margin: 20
+    },
+    contentText: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: '600'
     }
 })
 
